50ed0faa6294cbbc8026a1f3ae5df61c5182ade3
MJK-One <hanelejss012@naver.com>
2025-04-09 19:27:57 +0900

    [feat] 댓글 기능, 좋아요 구현 #8

--- src/main/java/com/newspeed/newspeed/NewspeedApplication.java
+++ src/main/java/com/newspeed/newspeed/NewspeedApplication.java
@@ -2,10 +2,11 @@ package com.newspeed.newspeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NewspeedApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NewspeedApplication.class, args);
 	}
---
+++ src/main/java/com/newspeed/newspeed/domain/comments/entity/Comment.java
@@ -0,0 +1,40 @@
+package com.newspeed.newspeed.domain.comments.entity;
+
+import com.newspeed.newspeed.common.entity.BaseTimeEntity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "Comment")
+@Data
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Comment extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(name = "text", columnDefinition = "TEXT")
+    private String commentText;
+
+    @Column(name = "likes", columnDefinition = "INT DEFAULT 0", nullable = false)
+    private Integer commentLikes;
+
+    // 생성자 추가
+    public Comment(User user, Post post, String commentText) {
+        this.user = user;
+        this.post = post;
+        this.commentText = commentText;
+    }
+}
