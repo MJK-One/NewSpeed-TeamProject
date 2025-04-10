a5a27511c7ce22c77c08f80925aa228c2dc0e7ff
yeeun0702 <kimyeeun0702@naver.com>
2025-04-10 21:05:30 +0900

    ✨ [feat] #11 회원 탈퇴 Controller 구현

--- src/main/java/com/newspeed/newspeed/domain/users/controller/UserController.java
+++ src/main/java/com/newspeed/newspeed/domain/users/controller/UserController.java
@@ -1,18 +1,20 @@
 package com.newspeed.newspeed.domain.users.controller;
 
+import com.newspeed.newspeed.common.exception.base.CustomException;
+import com.newspeed.newspeed.common.exception.code.enums.ErrorCode;
 import com.newspeed.newspeed.common.exception.code.enums.SuccessCode;
 import com.newspeed.newspeed.common.response.ApiResponseDto;
 import com.newspeed.newspeed.domain.users.dto.request.UserLoginRequestDto;
 import com.newspeed.newspeed.domain.users.dto.request.UserSignupRequestDto;
+import com.newspeed.newspeed.domain.users.dto.request.UserWithdrawRequestDto;
+import com.newspeed.newspeed.domain.users.entity.User;
 import com.newspeed.newspeed.domain.users.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -37,5 +39,26 @@ public class UserController {
         userService.login(userLoginRequestDto, httpServletRequest);
         return ResponseEntity.ok(ApiResponseDto.success(SuccessCode.USER_LOGIN_SUCCESS, "/api/users/login"));
     }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteUser(
+            @RequestBody @Valid UserWithdrawRequestDto requestDto,
+            HttpServletRequest httpServletRequest) {
+
+        HttpSession session = httpServletRequest.getSession(false);
+
+        // 세션에서 유저 꺼내기
+        User user = (User) session.getAttribute("user");
+
+        // 탈퇴 처리
+        userService.deleteUser(user.getUserId(), requestDto);
+
+        // 세션 무효화 (로그아웃 처리)
+        session.invalidate();
+
+        // 204 No Content
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
