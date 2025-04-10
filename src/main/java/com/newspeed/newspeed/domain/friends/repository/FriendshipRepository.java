66baa0b203f31898a00602f034c72e6e0891989e
SaltBr <snowgriff123@gmail.com>
2025-04-10 16:43:15 +0900

    [refactor] #5 클래스 이름을 대문자로 수정

--- src/main/java/com/newspeed/newspeed/domain/friends/repository/FriendshipRepository.java
+++ src/main/java/com/newspeed/newspeed/domain/friends/repository/FriendshipRepository.java
@@ -6,10 +6,10 @@ import org.springframework.data.repository.query.Param;
 
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
 
-    @Query("SELECT COUNT(f) FROM friendship f WHERE (f.followToId = :userId OR f.followFromId = :userId) AND f.status = 'ACCEPTED'")
+    @Query("SELECT COUNT(f) FROM Friendship f WHERE (f.followToId = :userId OR f.followFromId = :userId) AND f.status = 'ACCEPTED'")
     int countFriendsByUserId(@Param("userId") Long userId);
 
-    @Query("SELECT status FROM friendship f WHERE (f.followToId = :userId AND f.followFromId = :profileId) OR (f.followToId = :profileId AND f.followFromId = :userId)")
+    @Query("SELECT status FROM Friendship f WHERE (f.followToId = :userId AND f.followFromId = :profileId) OR (f.followToId = :profileId AND f.followFromId = :userId)")
     String findStatusByUserIdAndProfileId(@Param("userId") Long userId, @Param("profileId") Long profileId);
 
 }
