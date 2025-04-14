8419b8912ae54e52c9b7bce65c91e451fe4051ac
SaltBr <snowgriff123@gmail.com>
2025-04-14 09:14:19 +0900

    [fix] 유저 업데이트가 되지 않던 문제 수정

--- src/main/java/com/newspeed/newspeed/domain/profiles/dto/request/ProfileUpdateRequestDto.java
+++ src/main/java/com/newspeed/newspeed/domain/profiles/dto/request/ProfileUpdateRequestDto.java
@@ -3,24 +3,26 @@ package com.newspeed.newspeed.domain.profiles.dto.request;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ProfileUpdateRequestDto {
 
     @Size(max = 4, message = "최대 4글자까지 가능합니다.")
-    private final String name;
+    private String name;
     @Pattern(
             regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
             message = "올바른 이메일 형식이 아닙니다."
     )
-    private final String email;
+    private String email;
     @Size(min = 8, message = "비밀번호는 최소 8글자 이상이어야 합니다")
     @Pattern(
             regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+=-]).{8,}$",
             message = "비밀번호는 대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 포함되어야 합니다."
     )
-    private final String newPassword;
-    private final String previousPassword;
+    private String newPassword;
+    private String previousPassword;
 
     public ProfileUpdateRequestDto(String name, String email, String newPassword, String previousPassword) {
         this.name = name;
